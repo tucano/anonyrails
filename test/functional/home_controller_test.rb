b3a262da6091d1f4484889ed6f8086dac949bc6e
tucano <davide.rambaldi@gmail.com>
2009-02-07 15:54:06 +0100

    All index test are merged in a single test_index

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -10,23 +10,26 @@ class HomeControllerTest < ActionController::TestCase
 
   def test_index
     get :index
+    
     assert_response :success
-  end
-
-  def test_get_template
-    get :index
     assert_template 'home'
-  end
 
-  def test_page_response
-    get :index
+    assert assigns(:host)
+    assert assigns(:host_port)
+    assert assigns(:host_remote)
+    assert assigns(:host_remoteip)
+    assert assigns(:host_environment)
+
     assert_select "title"
+    
     assert_select "div#main" do
       assert_select "table#home-table"
     end
+    
     assert_select "div#request-environment" do
       assert_select "table#request-table"
     end
+
   end
 
 end
